c044d54817d671a1029afceacafb9774ac5ecde5
Alvin-Muchiri <169157608+Alvin-Muchiri@users.noreply.github.com>
2024-07-10 13:49:09 +0300

    Update programming semester project.cpp

--- programming semester project/programming semester project.cpp
+++ programming semester project/programming semester project.cpp
@@ -58,8 +58,7 @@ void addStudent()
              }
          }
      }
-        else if (choice == 2) 
-        {
+        else if (choice == 2) { 
             cout << "Choose club/society: ";
             for (size_t i = 0; i < clubs.size(); ++i) {
                 cout << i + 1 << ". " << clubs[i].name << " (" << clubs[i].members.size() << "/" << clubs[i].max_capacity << ")\n";
