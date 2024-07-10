72bb57a1f5f35a6a05d13bf5528d910f510b062b
Steve-Leone <steveleone2399@gmail.com>
2024-07-10 12:59:18 +0300

    Update programming semester project.cpp

--- programming semester project/programming semester project.cpp
+++ programming semester project/programming semester project.cpp
@@ -35,33 +35,4 @@ void addStudent() {
     cout << "Enter group (1,2,3): ";
     cin >> student.group;
 
-    int choice;
-    do {
-        cout << "Choose activity type: 1. Sport 2. Club/Society 3. Done: ";
-        cin >> choice;
-
-        if (choice == 1) {
-            cout << "Choose sport: ";
-            for (size_t i = 0; i < sports.size(); ++i) {
-                cout << i + 1 << ". " << sports[i].name << " (" << sports[i].members.size() << "/" << sports[i].max_capacity << ")\n";
-            }
-            int sport_choice;
-            cin >> sport_choice;
-            if (sport_choice >= 1 && sport_choice <= sports.size()) {
-                if (sports[sport_choice - 1].members.size() < sports[sport_choice - 1].max_capacity) {
-                    student.activities.push_back(sports[sport_choice - 1].name);
-                    sports[sport_choice - 1].members.push_back(student);
-                }
-                else {
-                    cout << "Sport is at full capacity.\n";
-                }
-            }
-        }
-        else if (choice == 2) {
-            cout << "Choose club/society: ";
-            for (size_t i = 0; i < clubs.size(); ++i) {
-                cout << i + 1 << ". " << clubs[i].name << " (" << clubs[i].members.size() << "/" << clubs[i].max_capacity << ")\n";
-            }
     
-
-
