4f73f3ae12f340038d2ebc73f4dfc6ddbc2f2fae
Steve-Leone <steveleone2399@gmail.com>
2024-07-10 12:57:42 +0300

    Update programming semester project.cpp

--- programming semester project/programming semester project.cpp
+++ programming semester project/programming semester project.cpp
@@ -12,4 +12,56 @@ struct Student {
     int group;
     vector<string> activities;
 };
+struct Activity {
+    string name;
+    int max_capacity;
+    vector<Student> members;
+};
+
+vector<Student> students;
+vector<Activity> sports = { {"Rugby", 20}, {"Athletics", 20}, {"Swimming", 20}, {"Soccer", 20} };
+vector<Activity> clubs = { {"Journalism Club", 60}, {"Red Cross Society", 60}, {"AISEC", 60}, {"Business Club", 60}, {"Computer Science Club", 60} };
+
+void addStudent() {
+    Student student;
+    cout << "Enter first name: ";
+    cin >> student.firstname;
+    cout << "Enter surname: ";
+    cin >> student.surname;
+    cout << "Enter gender (MUST BE Male/Female): ";
+    cin >> student.gender;
+    cout << "Enter age: ";
+    cin >> student.age;
+    cout << "Enter group (1,2,3): ";
+    cin >> student.group;
+
+    int choice;
+    do {
+        cout << "Choose activity type: 1. Sport 2. Club/Society 3. Done: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Choose sport: ";
+            for (size_t i = 0; i < sports.size(); ++i) {
+                cout << i + 1 << ". " << sports[i].name << " (" << sports[i].members.size() << "/" << sports[i].max_capacity << ")\n";
+            }
+            int sport_choice;
+            cin >> sport_choice;
+            if (sport_choice >= 1 && sport_choice <= sports.size()) {
+                if (sports[sport_choice - 1].members.size() < sports[sport_choice - 1].max_capacity) {
+                    student.activities.push_back(sports[sport_choice - 1].name);
+                    sports[sport_choice - 1].members.push_back(student);
+                }
+                else {
+                    cout << "Sport is at full capacity.\n";
+                }
+            }
+        }
+        else if (choice == 2) {
+            cout << "Choose club/society: ";
+            for (size_t i = 0; i < clubs.size(); ++i) {
+                cout << i + 1 << ". " << clubs[i].name << " (" << clubs[i].members.size() << "/" << clubs[i].max_capacity << ")\n";
+            }
+    
+
 
