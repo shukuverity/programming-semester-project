c28f3afb74103b4ed797b25e56229cab84e2baaf
Alvin-Muchiri <169157608+Alvin-Muchiri@users.noreply.github.com>
2024-07-10 13:31:05 +0300

    Update programming semester project.cpp

--- programming semester project/programming semester project.cpp
+++ programming semester project/programming semester project.cpp
@@ -56,4 +56,36 @@ void addStudent() {
              }
          }
      }
+        else if (choice == 2) {
+            cout << "Choose club/society: ";
+            for (size_t i = 0; i < clubs.size(); ++i) {
+                cout << i + 1 << ". " << clubs[i].name << " (" << clubs[i].members.size() << "/" << clubs[i].max_capacity << ")\n";
+            }
+            int club_choice;
+            cin >> club_choice;
+            if (club_choice >= 1 && club_choice <= clubs.size()) {
+                if (clubs[club_choice - 1].members.size() < clubs[club_choice - 1].max_capacity) {
+                    student.activities.push_back(clubs[club_choice - 1].name);
+                    clubs[club_choice - 1].members.push_back(student);
+                }
+                else {
+                    cout << "Club/Society is at full capacity.\n";
+                }
+            }
+        }
+    } while (choice != 3);
+
+    students.push_back(student);
+}
+
+void viewStudents() {
+    for (const auto& student : students) {
+        cout << "Name: " << student.firstname << " " << student.surname << ", Gender: " << student.gender << ", Age: " << student.age << ", Group: " << student.group << "\n";
+        cout << "Activities: ";
+        for (const auto& activity : student.activities) {
+            cout << activity << " ";
+        }
+        cout << "\n";
+    }
+}
 
